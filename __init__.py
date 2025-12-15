e70eb0ffd7adff6e1b8e878098a87a1538dac02f
Ashwin Panchapakesan <topgunzurhero@gmail.com>
2025-12-15 12:26:55 -0500

    It finally works

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from flake8_multiline_equals.checker import MultilineNamedArgsCheckerPlugin
 
-__all__ = ['MultilineNamedArgsCheckerPlugin']
+__all__ = ['MultilineNamedArgsCheckerPlugin', 'checker']
